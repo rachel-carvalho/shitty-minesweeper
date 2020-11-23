83e38b83ca57e347daacd6de7e6a898702cd4330
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-11-23 12:38:50 -0500

    export for deploy

--- out/_next/static/tFJv7a9i3BGjV5bXCa2Z5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fstyles.98af8310.chunk.css","static\u002Fchunks\u002Fstyles.ab6cbcfd2496381a1a52.js","static\u002Fchunks\u002Fpages\u002Findex-132f503766c672144a40.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ca6a368b57d4262d533d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
